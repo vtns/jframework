96a720c6504b871274efa39e3f76b1e8e7c219f3
tadayoshi <tadayoshi@MacBookProMid2014.local>
2018-10-20 05:57:28 +0900

    Initial Commit

---
+++ JLang/JLang.h
@@ -0,0 +1,19 @@
+//
+//  JLang.h
+//  JLang
+//
+//  Created by tadayoshi on 2018/10/20.
+//  Copyright © 2018 vtns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JLang.
+FOUNDATION_EXPORT double JLangVersionNumber;
+
+//! Project version string for JLang.
+FOUNDATION_EXPORT const unsigned char JLangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JLang/PublicHeader.h>
+
+
